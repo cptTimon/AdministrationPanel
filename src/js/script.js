687ae5369cafd5ff3e8aba799d0a90a693e3aa12
Wojciech Kaczmarzyński <wojciech.kaczmarzynski@gmail.com>
2021-01-21 13:27:32 +0100

    General and Banners ready

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,36 @@
 const hamburger = document.querySelector('.fas');
 const mobileNavUl = document.querySelector('.navbar-mobile ul');
-
-
+var isOpen = false;
+function toggleMobileMenu() {
+  if(!isOpen) {
+    mobileNavUl.classList.add('show');
+    mobileNavUl.classList.remove('hide');
+    isOpen = true;
+  } else {
+    mobileNavUl.classList.remove('show');
+    mobileNavUl.classList.add('hide');
+    isOpen = false;
+  }
+}
 hamburger.addEventListener('click', function(){
-  mobileNavUl.classList.toggle('show');
+  toggleMobileMenu();
 });
+
+
+const app = {
+
+  initPages: function() {
+    const thisApp = this;
+    thisApp.pages = document.querySelectorAll('.page');
+    console.log(thisApp.pages);
+    thisApp.activatePage(thisApp.pages[0].id);
+    console.log(thisApp.activatePage(thisApp.pages[0].id));
+  },
+
+  activatePage: function() {
+
+  }
+
+};
+
+app.initPages();
